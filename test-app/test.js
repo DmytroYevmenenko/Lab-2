29f848e5ce928d881b8fb74cacd1ab80da166803
Dmytro Yevmenenko <evmenenkooor@gmail.com>
2026-04-22 14:24:13 +0300

    1

--- test-app/test.js
+++ test-app/test.js
@@ -2,4 +2,4 @@ const { dayGenerator, startTimeout } = require('day-lib');
 
 console.log("--- Testing Day Generator Library ---");
 const gen = dayGenerator();
-startTimeout(gen, 5); // Работает 5 секунд
+startTimeout(gen, 5);
